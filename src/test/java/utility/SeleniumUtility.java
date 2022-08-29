57ed05621cc4484e060faa548d8c6e24e92a9428
vvvrao <vandhita.rao@dxc.com>
2022-08-29 13:51:25 +0530

    webdriver version hard push

--- src/test/java/utility/SeleniumUtility.java
+++ src/test/java/utility/SeleniumUtility.java
@@ -80,7 +80,7 @@ public class SeleniumUtility {
 
     	
 //    	System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
-    	WebDriverManager.chromedriver().setup();
+    	WebDriverManager.chromedriver().driverVersion("81.0.4044.138").setup();
     	ChromeOptions options = new ChromeOptions();
 
     	
