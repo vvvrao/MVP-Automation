dfaa8524d062ef390258b82794f5d6c8f9439478
vvvrao <vandhita.rao@dxc.com>
2022-08-29 13:42:19 +0530

    hardcode un pw

--- src/test/java/utility/SeleniumUtility.java
+++ src/test/java/utility/SeleniumUtility.java
@@ -40,30 +40,16 @@ public class SeleniumUtility {
     public static String password = generateRandomString(10);
     public SeleniumUtility() {
     	
-    	// We could use any driver for our tests...
-    	DesiredCapabilities capabilities = new DesiredCapabilities();
+    	
 
-    	// ... but only if it supports javascript
-    	capabilities.setJavascriptEnabled(true);
-
-    	// Get a handle to the driver. This will throw an exception
-    	// if a matching driver cannot be located
-    	WebDriver driver = new RemoteWebDriver(capabilities);
-
-    	// Query the driver to find out more information
-    	Capabilities actualCapabilities = ((RemoteWebDriver) driver).getCapabilities();
-
-    	// And now use it
-    	driver.get("http://www.google.com");
-
-//    	WebDriverManager.chromedriver().setup();
-//    	
-//    	
-//    	 
-//    	driver = new ChromeDriver();
-//        this.driver= driver;
-//        // Initialize Ajax page initialisation
-//        PageFactory.initElements(driver, this);
+    	WebDriverManager.chromedriver().setup();
+    	
+    	
+    	 
+    	driver = new ChromeDriver();
+        this.driver= driver;
+        // Initialize Ajax page initialisation
+        PageFactory.initElements(driver, this);
 
     }
 
