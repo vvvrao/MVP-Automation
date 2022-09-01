7cc7051da192a6a78f5cfeba66e3af868242032e
vvvrao <vandhita.rao@dxc.com>
2022-09-01 17:47:48 +0530

    pipeline6

--- src/test/java/runner/runnerIT.java
+++ src/test/java/runner/runnerIT.java
@@ -14,12 +14,11 @@ import static utility.SeleniumUtility.driver;
 
 import java.util.function.Predicate;
 
-import com.hpe.alm.octane.OctaneCucumber;
 
-@RunWith(OctaneCucumber.class)
+
+@RunWith(Cucumber.class)
 @CucumberOptions(features ="src/test/java/resources",
-            plugin = {"com.hpe.alm.octane.OctaneGherkinFormatter:gherkin-results/ManualRunnerTest_OctaneGherkinResults.xml",
-        "html:target/cucumber.html","json:target/cucumber.json"},
+            plugin = {"html:target/cucumber.html","json:target/cucumber.json"},
         monochrome = true,
         glue ={"glue"})
 
