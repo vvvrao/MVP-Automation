705dd4b69d1fca09300b9c42740df431b6509e87
vvvrao <vandhita.rao@dxc.com>
2022-09-08 19:56:31 +0530

    registeration

--- src/test/java/runner/runnerIT.java
+++ src/test/java/runner/runnerIT.java
@@ -12,10 +12,9 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(plugin={"pretty","json:target/cucumber.json","html:target/cucumber.html","junit:target/cucumber.xml"},
-features ="src/test/java/resources/",
+features ="src/test/java/resources/1User Registration Feature_12001.feature",
         monochrome = true,
-        glue ={"glue","hooks"},
-tags= "@TID12001REV0.7.0 or @TID13001REV0.5.0 or @TID12002REV0.9.0")
+        glue ={"glue","hooks"})
 
 
 
