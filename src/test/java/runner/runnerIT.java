686685c9939193d4103f85e269b3099fe645155f
vvvrao <vandhita.rao@dxc.com>
2022-09-12 11:37:10 +0530

    1

--- src/test/java/runner/runnerIT.java
+++ src/test/java/runner/runnerIT.java
@@ -12,7 +12,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(plugin={"pretty","json:target/cucumber.json","html:target/cucumber.html","junit:target/cucumber.xml"},
-features ="src/test/java/resources/",
+features ="src/test/java/resources/1User Registration Feature_12001.feature",
         monochrome = true,
         glue ={"glue","hooks"})
 
